d9bf4a72c1a1c467723257abc7dd0d318e375f2e
Shuai Huang <shhuang9532@gmail.com>
2020-10-31 14:54:50 -0700

    init code for spring boot

---
+++ src/test/java/com/group5/BookRead/BookReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.BookRead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
